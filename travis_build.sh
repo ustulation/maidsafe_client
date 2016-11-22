1cea2cf00e33261b6ff736de987015e149be189c
ustulation <ustulation@gmail.com>
2016-11-22 17:22:41 +0000

    chore/ci: update ci

--- travis_build.sh
+++ travis_build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Show expanded commands while running
-set -x
-
 # Stop the script if any command fails
-set -e 
-set -o pipefail
+set -o errtrace
+trap 'exit' ERR
 
 cd $TRAVIS_BUILD_DIR
 
@@ -13,3 +10,6 @@ if [[ $TRAVIS_RUST_VERSION = stable ]]; then
   cargo test --release --no-run
   cargo test --release --features use-mock-routing
 fi
+
+# Hide expanded commands while running
+set +x
